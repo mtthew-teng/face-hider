fbb456d0bce6088f32179a75f435b8a96cc7a035
mtthew-teng <196126034+mtthew-teng@users.noreply.github.com>
2025-02-09 21:08:38 -0800

    added video blurrer

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import os
 import cv2
 import mediapipe as mp
+import argparse
 
 def process_img(img, face_detection):
+    H, W, _ = img.shape
+    
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     out = face_detection.process(img_rgb)
 
@@ -21,19 +25,50 @@ def process_img(img, face_detection):
 
     return img
 
-img_path = './data/girlface3.jpg'
+args = argparse.ArgumentParser()
+args.add_argument("--mode", default='video')
+args.add_argument("--filePath", default='./data/girlface2.mov')
+args = args.parse_args()
 
-img = cv2.imread(img_path)
-
-# img = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-
-H, W, _ = img.shape
+output_dir =  './output'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
 mp_face_detection = mp.solutions.face_detection
 
-while True:
-    with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
+with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
+    if args.mode in ["image"]:
+        img = cv2.imread(args.filePath)
+        
+
         img = process_img(img, face_detection)
 
-img.release()
-cv2.destroyAllWindows()
+        cv2.imwrite(os.path.join(output_dir, 'output.png'), img)
+
+    elif args.mode in ["video"]:
+        cap = cv2.VideoCapture(args.filePath)
+        ret, frame = cap.read()
+
+        (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+
+        if int(major_ver)  < 3 :
+            fps = cap.get(cv2.cv.CV_CAP_PROP_FPS)
+            print ("Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps))
+        else :
+            fps = cap.get(cv2.CAP_PROP_FPS)
+            print ("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
+
+        output_video = cv2.VideoWriter(os.path.join(output_dir, 'output.mp4'), 
+                                       cv2.VideoWriter_fourcc(*'MP4V'),
+                                       fps,
+                                       (frame.shape[1], frame.shape[0]))
+
+        while ret:
+            frame = process_img(frame, face_detection)
+            
+            output_video.write(frame)
+
+            ret, frame = cap.read()
+
+        cap.release()
+        output_video.release()
